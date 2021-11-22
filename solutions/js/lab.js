94d257684c34f2eb3a51bb9f239e4cada7588b17
Sorantes <91084413+Sorantes@users.noreply.github.com>
2021-11-22 14:44:49 -0800

    Updating the the questions and answers

--- solutions/js/lab.js
+++ solutions/js/lab.js
@@ -24,9 +24,39 @@ function yourFunction() {
 }
 
 
-//The first option for Question 1
+//The first option for Question 2
 function hisFunction() {
   var text = document.getElementById("operation");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+//The second option for Question 2
+function herFunction() {
+  var text = document.getElementById("operationtwo");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+//The first option for Question 3
+function theirFunction() {
+  var text = document.getElementById("procedure");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+//the second option for Question 3
+function ourFunction() {
+  var text = document.getElementById("proceduretwo");
   if (text.style.display === "none") {
     text.style.display = "block";
   } else {
