7b9d32442e2d328f954f94ab0e27f02af59e8103
Sorantes <91084413+Sorantes@users.noreply.github.com>
2021-11-21 19:25:08 -0800

    just switched it to the new repository

---
+++ solutions/js/lab.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+//The first option for Question 1
+function myFunction() {
+  var text = document.getElementById("test");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+//The second option for Question 1
+function yourFunction() {
+  var text = document.getElementById("testtwo");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+
+//The first option for Question 1
+function hisFunction() {
+  var text = document.getElementById("operation");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
